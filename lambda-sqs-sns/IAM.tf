84cf6dbd451d0bcc25cb46e8b0729066b5c4a40b
Jiku Sandilya <jikushandilya@gmail.com>
2023-08-29 17:23:08 +0530

    changes

---
+++ lambda-sqs-sns/IAM.tf
@@ -0,0 +1,12 @@
+// IAM - Identity and Access Management
+// IAM users and groups
+// users are people in the organization
+// groups can be deifferent team in a organization
+// users can belong to multiple groups (or it may not belong to any group)
+// users or groups can be attached with IAM policies which give them the permission
+// we need to give least priviledge
+
+
+// IAM Roles
+// Roles need to be attached with services to perform action
+// e.g. Glue Role, Lambda Role etc.
